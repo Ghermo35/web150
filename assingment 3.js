7815732f6237a163f188877286f1ad0840893be4
Ghermo35 <43656152+Ghermo35@users.noreply.github.com>
2020-06-09 18:42:52 -0700

    Add files via upload

---
+++ assingment 3.js
@@ -0,0 +1,17 @@
+'use strict'
+/* The script is placed inside an immediately invoked function expression
+   which helps protect the scope of variables */
+
+   //constructor notation with properties and methods
+function Car (make, model, year, color){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.start = function(){
+        var vroom = 'Vroom Vroom';
+        return vroom;
+    }
+}
+    var myCar = new Car("toyora", "camry", "2020", "Red");
+    var myCar2 = new Car("tesla", "x", "2019", "grey");
